86b81866ddba7b31311647a638ffb617431005f6
Juan David Murillo Mejia <juandavidmurillomejia0718@outlook.es>
2025-01-29 16:52:56 -0500

    29/01/2025 01

--- Ejercicios-De-Python/03-Condicionales.py
+++ Ejercicios-De-Python/03-Condicionales.py
@@ -5,21 +5,52 @@
 
 # Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
 
+edad = input("Cual es su edad: ")
+
+if edad < 18:
+    print("Eres menor de edad")
+else:
+    print("eres mayor de edad")
 
 # Ejercicio 2
+
 # Escribir un programa que almacene la cadena de caracteres contraseña en una variable, pregunte al usuario por la contraseña e imprima por pantalla si la contraseña introducida por el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas.
 
+key = "contraseña"
+password = input("Introduce la contraseña: ")
+
+if key == password.lower():
+    print("La contaseña coincide")
+else:
+    print("La contraseña no coincide")
 
 # Ejercicio 3
+
 # Escribir un programa que pida al usuario dos números y muestre por pantalla su división. Si el divisor es cero el programa debe mostrar un error.
+
+n = float(input("Introduce el dividendo: "))
+m = float(input("Introduce el divisior: "))
+if m == 0:
+    print("¡Error! No se puede dividir por 0.")
+else:
+    print(n/m)
 
 
 # Ejercicio 4
+
 # Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par o impar.
 
+n = int(input("Introduce un número entero: "))
+
+if n % 2 == 0:
+    print("El número " + str(n) + " es par")
+else:
+    print("El número " + str(n) + " es impar")
 
 # Ejercicio 5
+
 # Para tributar un determinado impuesto se debe ser mayor de 16 años y tener unos ingresos iguales o superiores a 1000 € mensuales. Escribir un programa que pregunte al usuario su edad y sus ingresos mensuales y muestre por pantalla si el usuario tiene que tributar o no.
+
 
 
 # Ejercicio 6
